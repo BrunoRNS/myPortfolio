ea5c38a5160e70d079290450b0ab44fface15dea
BrunoRNS <brunorns05@outlook.com>
2025-02-24 16:23:19 -0300

    Add new script.js and update style.css and index.html
    - Added a new JavaScript file `script.js` to enhance frontend functionality.
    - Updated `style.css` to improve the responsive design.
    - Modified `index.html` to integrate new styles and scripts.

---
+++ home/static/home/script.js
@@ -0,0 +1,7 @@
+function dropbox() {
+    if (document.getElementById("dropdown-content").className == "show") {
+        document.getElementById("dropdown-content").className = "hidden";
+    } else {
+        document.getElementById("dropdown-content").className = "show";
+    }
+}
