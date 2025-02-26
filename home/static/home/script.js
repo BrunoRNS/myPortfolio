75ea9a31dc813dea6f709894ccf8f3d4d1493354
BrunoRNS <brunorns05@outlook.com>
2025-02-26 17:59:25 -0300

    Remove unused script.js, update style.css and index.html for responsiveness
    - Deleted `home/static/home/script.js` as it is no longer needed.
    - Modified `home/static/home/style.css` to improve responsiveness.
    - Updated `home/templates/home/index.html` to enhance layout on various devices.

--- home/static/home/script.js
+++
@@ -1,7 +0,0 @@
-function dropbox() {
-    if (document.getElementById("dropdown-content").className == "show") {
-        document.getElementById("dropdown-content").className = "hidden";
-    } else {
-        document.getElementById("dropdown-content").className = "show";
-    }
-}
