8f04f6a4d85f9f96f399d6cd32d1cefb7c82c991
BrunoRNS <brunorns05@outlook.com>
2025-07-21 15:51:23 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+PORT=${PORT:-8080}
+
+sed -i "s/listen 8080;/listen $PORT;/" /etc/nginx/nginx.conf
+
+mkdir -p /run/nginx
+chown nginxuser:nginxgroup /run/nginx
+
 /opt/venv/bin/gunicorn \
     --bind 0.0.0.0:8000 \
     --workers 3 \
