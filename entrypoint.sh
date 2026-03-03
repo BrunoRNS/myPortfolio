cc2c5a718508bce3d8b1b6f6f8416e598a23a109
BrunoRNS <brunorns05@outlook.com>
2026-03-03 18:58:21 -0300

    Add manual bash tcp instead of nc entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,26 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
-/opt/venv/bin/gunicorn \
-    server.asgi:application \
+echo "Starting Gunicorn..."
+gunicorn server.asgi:application \
     --bind 127.0.0.1:8000 \
     --workers 2 \
     --worker-class uvicorn.workers.UvicornWorker \
     --max-requests 1000 \
     --max-requests-jitter 50 &
 
-echo "Waiting Gunicorn to start..."
-while ! nc -z 127.0.0.1 8000; do
-  sleep 1
-done
-echo "Gunicorn its ready, starting nginx..."
+wait_for_port() {
+    local host=$1 port=$2 timeout=$3
+    local start_time=$(date +%s)
+    while ! timeout 1 bash -c "echo > /dev/tcp/$host/$port" 2>/dev/null; do
+        sleep 1
+        if (($(date +%s) - start_time >= timeout)); then
+            echo "Timeout waiting $host:$port"
+            exit 1
+        fi
+    done
+}
 
+wait_for_port 127.0.0.1 8000 30
+echo "Gunicorn ready, starting nginx..."
 exec nginx -g "daemon off;"
