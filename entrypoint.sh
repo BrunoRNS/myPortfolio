f085168ec07939b450d1d4505c513ecfb9210f7f
BrunoRNS <brunorns05@outlook.com>
2026-03-03 18:45:58 -0300

    Fix 502 bad gateway

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 set -e
 
- /opt/venv/bin/gunicorn \
+/opt/venv/bin/gunicorn \
     server.asgi:application \
     --bind 127.0.0.1:8000 \
     --workers 2 \
     --worker-class uvicorn.workers.UvicornWorker &
+
+echo "Waiting Gunicorn to start..."
+while ! nc -z 127.0.0.1 8000; do
+  sleep 1
+done
+echo "Gunicorn its ready, starting nginx..."
 
 exec nginx -g "daemon off;"
